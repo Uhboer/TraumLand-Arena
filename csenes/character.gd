4798802c5c876289bd6e266a485e5ea200019f1e
Uhboer <grishukoleg258@gmail.com>
2025-03-01 16:53:20 +0300

    thats better

--- csenes/character.gd
+++ csenes/character.gd
@@ -1,9 +1,22 @@
 extends CharacterBody2D
 
+@onready var anim = $layouts/body
+
+
 var speed = 200  
 
 func _physics_process(delta):
 	var direction = Input.get_vector("A", "D", "W", "S")
-	velocity = direction * speed
+	if direction:
+		anim.play("Run")
+		velocity = direction * speed
+	else:
+		velocity = Vector2(0, 0)
+		anim.play("Idle")
+	if Input.is_action_just_pressed("A"):
+		anim.flip_h = true
+	if Input.is_action_just_pressed("D"):
+		anim.flip_h = false
+
 
 	move_and_slide()
